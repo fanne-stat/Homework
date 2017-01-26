bec636cad37b43d9877c0940c75eebba783a63ce
Yifan Zhu <ifanzhu@foxmail.com>
2017-01-26 08:56:48 -0600

    problem 5 value check

---
+++ STAT580/STAT580hw1YifanZhu/codes/p5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	int i = 0, j = 2;
+	printf("%d\n", 3/2 + 1);
+	printf("%f\n", 1/2 + 3.0 / 4.0);
+	printf("%d\n", 3 < 10 && 2 * 8 < 2);
+	printf("%d\n", x = 3 + 2);
+	printf("%d\n", j+=i++);
+	printf("%d\n", i);
+	return 0;
+}
